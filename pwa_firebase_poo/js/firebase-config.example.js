51ce5c7644711dfc88ab5477fbebddd5326fa133
Malu-git-11 <malumendesrodrigues13@gmail.com>
2025-11-05 11:21:29 -0300

    PWA_com_Firebase
    Criação de uma pwa com tela de cadastro e login usando firebase, falta criação da tela de perfil e hospedar aplicação.

---
+++ pwa_firebase_poo/js/firebase-config.example.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "SUA_API_KEY",
+  authDomain: "SEU_PROJETO.firebaseapp.com",
+  projectId: "SEU_PROJETO",
+  storageBucket: "SEU_PROJETO.appspot.com",
+  messagingSenderId: "SENDER_ID",
+  appId: "APP_ID"
+};
